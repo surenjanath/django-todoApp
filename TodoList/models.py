813215d3d06f051a7d989f405de96985c3aee0f7
Surenjanath Singh <surenjanath.singh@gmail.com>
2021-09-24 19:59:19 -0400

    Looped through items and added each items with link

--- TodoList/models.py
+++ TodoList/models.py
@@ -5,6 +5,23 @@ from django.db import models
 # from django.contrib.admin.models import LogEntry
 # LogEntry.objects.all().delete()
 
+
+# If error such as no table found use the following sets
+'''
+1. delete db.sqlite3
+2. delete migration
+
+3. Create and run the migrations:
+
+python manage.py makemigrations
+python manage.py migrate
+
+4. Sync the database:
+manage.py migrate --run-syncdb
+
+'''
+
+
 # Create your models here.
 class TodoTask(models.Model):
     text        = models.CharField(max_length = 45)
--- TodoList/views.py
+++ TodoList/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import TodoTask
 # Create your views here.
 def Home(request) :
-    return render(request,'TodoList/home.html')
+    todo_items = TodoTask.objects.order_by('id')
+    context = {'Todo_items' : todo_items}
+    return render(request,'TodoList/home.html', context)
