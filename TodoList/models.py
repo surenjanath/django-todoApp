162567369ed6fa9dc3ab29d97ceeac4b232ab4ed
Surenjanath Singh <surenjanath.singh@gmail.com>
2021-09-24 16:56:15 -0400

    First commit :  with templates and model - Todolist Website

---
+++ TodoList/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# To clear data from recent actions use :
+# python manage.py shell
+# from django.contrib.admin.models import LogEntry
+# LogEntry.objects.all().delete()
+
+# Create your models here.
+class Task(models.Model):
+    text        = models.CharField(max_length = 45)
+    completed   = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.text
