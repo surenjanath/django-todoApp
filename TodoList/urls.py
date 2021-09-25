8fb3ad27b1f035b9cebe77b98f5be8390ddfbc53
Surenjanath Singh <surenjanath.singh@gmail.com>
2021-09-24 20:45:28 -0400

    Added form to gather data and implemented button functions

--- TodoList/urls.py
+++ TodoList/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name = 'Home')
+    path('', views.Home, name = 'Home'),
+    path('add', views.addTodoItem, name = 'Add'),
+    path('completed/<todo_id>', views.completedTodo, name = 'Completed'),
+    path('deletecompleted', views.deleteCompleted, name = 'delete'),
+    path('deleteall', views.deleteAll, name = 'deleteAll'),
+
 
 ]
--- TodoList/views.py
+++ TodoList/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TodoTask
+from .forms import TodoListForm
+from django.views.decorators.http import require_POST
 # Create your views here.
 def Home(request) :
     todo_items = TodoTask.objects.order_by('id')
-    context = {'Todo_items' : todo_items}
+
+    form    = TodoListForm()
+
+    context = {'Todo_items' : todo_items,
+               'form'      : form}
+
     return render(request,'TodoList/home.html', context)
+
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+    if form.is_valid():
+        new_todo = TodoTask(text = request.POST['text'])
+        new_todo.save()
+    return redirect('Home')
+
+
+def completedTodo(request, todo_id):
+    todo = TodoTask.objects.get(pk = todo_id)
+    todo.completed = True
+    todo.save()
+    return redirect('Home')
+
+def deleteCompleted(request):
+    todo = TodoTask.objects.filter(completed__exact = True).delete()
+
+    return redirect('Home')
+
+def deleteAll(request):
+    todo = TodoTask.objects.all().delete()
+
+    return redirect('Home')
